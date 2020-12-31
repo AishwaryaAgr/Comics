7aea796e43c052bd1ad36e00016b8b4e032d6cf5
Aishwarya <aishwaryaagrawal1211@gmail.com>
2020-12-31 10:21:32 +0530

    COmmiting

---
+++ script.js
@@ -0,0 +1,6 @@
+const p1_right= document.querySelector('page-1-right');
+const p1_content = document.querySelector('page-1-content');
+p1_right.addEventListener('fullscreenchange', function(p1_content){
+    return p1_content.innerHTML = `<div>Do it</div>`;
+
+})
